579094181ddd02c837acc072692b6b41d6567a8f
YossiCharash <c0548508540@gmail.com>
2025-12-30 10:56:09 +0200

    test

--- frontend/public/env-config.js
+++
@@ -1,3 +0,0 @@
-window.ENV = window.ENV || {}
-window.ENV.API_URL = window.ENV.API_URL || 'http://localhost:8000/api/v1'
-
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,5 +1,11 @@
 /// <reference types="vite/client" />
 
+interface Window {
+  _env_: {
+    VITE_API_URL?: string
+  }
+}
+
 interface ImportMetaEnv {
   readonly VITE_API_URL?: string
 }
